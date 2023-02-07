65e589a4d0e37718fbb6b1c270e76fdd421c05b4
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2023-02-07 09:06:26 -0300

    fix packages

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -57,39 +57,67 @@ const PostPage: React.FC = () => {
         <div className="flex justify-between w-full gap-8 py-4">
           <div className="flex flex-1 flex-col overflow-y-auto py-2">
             <h1 className="text-3xl font-bold">{post?.title}</h1>
-            <p className="leading-relaxed p-4 text-lg">{post.description}</p>
+            <p className="leading-relaxed p-4 text-lg">
+              {post.description} Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Saepe illum deleniti, commodi nesciunt
+              exercitationem tempora error vitae inventore recusandae
+              necessitatibus veritatis dolor odio dolorem a impedit. Praesentium
+              non at incidunt numquam voluptatibus repellat. Aliquid deserunt
+              officia impedit minima minus esse obcaecati facere laboriosam,
+              dolorem itaque aspernatur voluptas quo error dicta! Facilis eius
+              non eveniet velit! Voluptates sint possimus iusto molestiae
+              aliquid tenetur repellendus eius ab minus ipsum! Nihil veniam
+              necessitatibus nesciunt unde tempore illo, laboriosam sequi, enim
+              doloremque dolorem, aliquam ut est cumque repellendus laudantium
+              nulla reiciendis mollitia! Quos optio dolores, repellat iste sit
+              reiciendis rem vel asperiores velit tempore ab, recusandae cum
+              consequuntur debitis expedita modi blanditiis nihil cupiditate.
+              Enim quis iusto facere eveniet eos totam ad dolore aliquid.
+              Asperiores, sit possimus. Deleniti quis fuga inventore accusantium
+              asperiores tenetur, perferendis quisquam rem dicta veritatis,
+              provident consectetur natus magnam sint! Maxime incidunt expedita
+              beatae veniam nihil itaque, distinctio soluta quas culpa omnis
+              architecto accusantium maiores, blanditiis mollitia illo eligendi,
+              praesentium voluptatibus dolor ullam! Ut dignissimos doloremque
+              magni corporis odio dolore deleniti molestias dolores, nihil
+              aspernatur! Autem, id ab odit nesciunt aperiam ex aliquid impedit
+              at? Suscipit quae eius dolor laborum quidem veritatis amet maxime
+              alias ad numquam? Ipsum, neque consequuntur?
+            </p>
           </div>
-          <div className="bg-slate-800 w-[320px] max-h-[200px] p-4 rounded-md shadow-lg">
-            <h3 className="font-bold text-lg">Detalhes da postagem</h3>
-            <ul className="flex flex-col p-2 gap-2">
-              <li className="flex items-center gap-2">
-                <PencilSimpleLine size={20} weight="bold" />
-                <span>{post.user?.name || ""}</span>
-              </li>
-              <li className="flex items-center gap-2">
-                <Clock size={20} weight="bold" />
-                <span>
-                  {format(
-                    new Date(post?.createdAt),
-                    "EEEE' • 'd' de 'MMMM' • 'k'h'm",
-                    { locale: ptBR }
-                  )}
-                </span>
-              </li>
-              <li className="flex items-center gap-2">
-                <Heart
-                  size={20}
-                  weight={likedPost}
-                  onClick={handleLiked}
-                  className="cursor-pointer"
-                />
-                <span>{post?.likes.length}</span>
-              </li>
-              <li className="flex items-center gap-2">
-                <ChartLineUp size={20} weight="bold" />
-                <span>{post?.evaluation}</span>
-              </li>
-            </ul>
+          <div className="w-[320px] h-full">
+            <div className="w-full h-auto bg-slate-800 p-4 rounded-md shadow-lg">
+              <h3 className="font-bold text-lg">Detalhes da postagem</h3>
+              <ul className="flex flex-col p-2 gap-2">
+                <li className="flex items-center gap-2">
+                  <PencilSimpleLine size={20} weight="bold" />
+                  <span>{post.user?.name || ""}</span>
+                </li>
+                <li className="flex items-center gap-2">
+                  <Clock size={20} weight="bold" />
+                  <span>
+                    {format(
+                      new Date(post?.createdAt),
+                      "EEEE' • 'd' de 'MMMM' • 'k'h'm",
+                      { locale: ptBR }
+                    )}
+                  </span>
+                </li>
+                <li className="flex items-center gap-2">
+                  <Heart
+                    size={20}
+                    weight={likedPost}
+                    onClick={handleLiked}
+                    className="cursor-pointer"
+                  />
+                  <span>{post?.likes.length}</span>
+                </li>
+                <li className="flex items-center gap-2">
+                  <ChartLineUp size={20} weight="bold" />
+                  <span>{post?.evaluation}</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       ) : (
